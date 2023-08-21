1252cef5b2a7035fd6c6651b58accc025974c76d
Samoth0621 <thomasguerri@gmail.com>
2023-08-21 21:49:34 +0200

    correction formulaire login

--- assets/js/components/Login.jsx
+++ assets/js/components/Login.jsx
@@ -40,21 +40,6 @@ export default function Login(props) {
                     Vos identifiants sont incorrects
                 </div>
             }
-            <div className="container-form">
-            <form onSubmit={formSubmit}>
-                <div className="form-group">
-                <input type="email" className="form-control" id="auth_email" name="auth_email" defaultValue="" placeholder="Email" />
-                </div>
-                <div className="form-group">
-                <input type="password" className="form-control" id="auth_password" name="auth_password" defaultValue="" placeholder="Mot de passe" />
-                </div>
-                <a href="/register" className="d-block text-white">Je n'ai pas de compte, je m'inscris</a>
-                <input type="hidden" id="template" name="template" defaultValue="login_form" />
-                <input type="hidden" id="subject" name="subject" defaultValue="Connexion Happy Voisin" />
-		        <input type="hidden" id="auth_action" name="auth_action" defaultValue="login" />
-                <button type="submit" className="rounded-pill px-3 btn btn-sm bg-orange text-white big-button">Je me connecte</button>
-            </form>
-            </div>
         </>
     );
 }
